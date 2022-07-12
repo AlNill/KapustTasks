a6b870d6fe4087948a7a734b0550dae530f77386
alek <user@user>
2022-07-12 11:33:17 +0300

    5-th task ??

---
+++ TransformToElephant/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		TransformToElephant();
+		Console.WriteLine("Fly");
+		//... custom application code
+	}
+
+	static void TransformToElephant()
+	{
+		string el = "Elephant";
+		if (Console.Out.NewLine != el)
+        {
+			Console.WriteLine(el);
+			Console.SetOut(new StringWriter() { NewLine = "" });
+		}
+	}
+}
