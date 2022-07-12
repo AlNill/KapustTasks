57a3726644a725141dca884d667e8a39e76a4ce5
user <user@DESKTOP-UEB9NA2>
2022-07-12 10:21:15 +0300

    Add project files.

---
+++ FailProcess/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+	static void Main(string[] args)
+	{
+		try
+		{
+			FailProcess();
+		}
+		catch { }
+
+		Console.WriteLine("Failed to fail process!");
+		Console.ReadKey();
+	}
+
+	static void FailProcess()
+	{
+		Console.WriteLine("If you see only this text - success fail process!");
+		Environment.Exit(0);
+	}
+}
